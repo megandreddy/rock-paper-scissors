990a43bee741cbe0888e8b569b9c9160c07f510f
megandreddy <87030448+megandreddy@users.noreply.github.com>
2021-07-14 16:23:15 -0400

    Generate computer choice

--- game.py
+++ game.py
@@ -1,6 +1,19 @@
 # game.py
 
+import random
+import os
+from dotenv import load_dotenv
+
+load_dotenv() #> loads contents of the .env file into the script's environment
+
+z = os.getenv("USER_NAME")
+
 print("Rock, Paper, Scissors, Shoot!")
+
+
+
+
+#exit()
 
 #
 # ASK FOR A USER INPUT
@@ -19,9 +32,35 @@ else:
     exit()
 
 
-print("LATER MESSAGES")
+
+
+
+
+
+
+
+
+
+# print("LATER MESSAGES")
+
+print("USER CHOSE:", x)
+
 
 # GENERATE A COMPUTER CHOICE
+# source: https://stackoverflow.com/questions/306400/how-to-randomly-select-an-item-from-a-list
+#
+# import random
+#
+# foo = ['a', 'b', 'c', 'd', 'e']
+# print(random.choice(foo))
+
+
+
+valid_options = ["rock", "paper", "scissors"] # list
+
+c = random.choice(valid_options)
+
+print("COMPUTER CHOSE:", c)
 
 
 # DETERMINE THE WINNER
