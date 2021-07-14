46012c33ce673bc3868a3f45a30d4d603423da37
megandreddy <87030448+megandreddy@users.noreply.github.com>
2021-07-14 12:39:45 -0400

    validating user inputs

--- game.py
+++ game.py
@@ -7,11 +7,21 @@ print("Rock, Paper, Scissors, Shoot!")
 # source: https://docs.python.org/3/library/functions.html#input
 #s = input('--> ')
 
-x = input("please choose one of 'rock', 'paper', 'scissors'")
+x = input("please choose one of 'rock', 'paper', 'scissors'; ")
 print(x)
 # VALIDATE USER INPUT
 
-# GENERATE COMPUTER CHOICE
+
+if (x == "rock") or (x == "paper") or (x == "scissors"):
+    print("VALID")
+else:
+    print("OOPS, INVALID, PLEASE TRY AGAIN")
+    exit()
+
+
+print("LATER MESSAGES")
+
+# GENERATE A COMPUTER CHOICE
 
 
 # DETERMINE THE WINNER
